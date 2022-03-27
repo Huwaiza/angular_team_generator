58e8fa16028f5e5e099696233cba4f641ebc6cdb
Huwaiza <muhammadhuwaizatahir@gmail.com>
2022-03-27 17:31:52 +0500

    logic implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
   newMemberName = "";
   members: string[] = [];
   errorMessage = ""
-
+  numberOfTeams: number | "" = ""
+  teams: string[][] = []
   addMember() {
 
     if(!this.newMemberName){
@@ -23,5 +24,33 @@ export class AppComponent {
 
   onInput(member: string){
     this.newMemberName = member
+  }
+
+  onNumberTeamInput(teamNumber: String) {
+    this.numberOfTeams = Number(teamNumber)
+  }
+
+  generateTeams(){
+    if(!this.numberOfTeams  || this.numberOfTeams <= 0){
+      this.errorMessage = "Please enter postive digit for number of teams"
+      return;
+    }
+    const allMembers = [...this.members]
+    this.errorMessage  = ""
+
+    while(allMembers.length){
+      for (let i = 0; i < this.numberOfTeams; i++){
+        const randomIndex = Math.floor(Math.random() * allMembers.length)
+        const member = allMembers.splice(randomIndex, 1)[0]
+        
+        if (!member)break;
+        if (this.teams[i]){
+          this.teams[i].push(member)
+        }else{
+          this.teams[i] = [member]
+        }
+      }
+    }
+    console.log(this.teams)
   }
 }
